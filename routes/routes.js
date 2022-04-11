ac0b3f94bc03a1335dac81654f1f3f5c6430d37c
mateussluan <luanmateuscd@gmail.com>
2022-04-11 17:00:38 -0300

    getbyid, editar e deletar

--- routes/routes.js
+++ routes/routes.js
@@ -4,5 +4,9 @@ const TaskController = require("../controller/TaskController");
 //Exportando o método
 routes.get("/", TaskController.getAllTasks);
 routes.post("/create", TaskController.createTask);
+routes.get("/getById/:id/:method", TaskController.getById);
+routes.post("/updateOne/:id", TaskController.updateOneTask);
+routes.get("/deleteOne/:id", TaskController.deleteOneTask);
+
 
 module.exports = routes;
