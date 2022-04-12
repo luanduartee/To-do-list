d7bd6e3f61de90cc8d6b5a3e19125052a64f6b6a
mateussluan <luanmateuscd@gmail.com>
2022-04-12 18:52:15 -0300

    Deploy Configs

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectToDb = () => {
   mongoose
     .connect(
-      "mongodb+srv://root:jesuse100@todolist.supr9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+      process.env.DB_URI,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
